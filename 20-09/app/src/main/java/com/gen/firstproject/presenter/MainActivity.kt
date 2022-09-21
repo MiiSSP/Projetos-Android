b55af2d6bf152639d8d3c77db5b764dc3fd2eb34
MiiSSP <milena.souzasp@hotmail.com>
2022-09-21 20:02:48 -0300

    Criando primeiro layout no Android Studio

---
+++ 20-09/app/src/main/java/com/gen/firstproject/presenter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gen.firstproject.presenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gen.firstproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
