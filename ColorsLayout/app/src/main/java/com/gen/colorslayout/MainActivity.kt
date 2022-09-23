ebb89291871ac0366805d0c2bbf07c52f15a1394
MiiSSP <milena.souzasp@hotmail.com>
2022-09-23 01:26:48 -0300

    Criando layout de colors app

---
+++ ColorsLayout/app/src/main/java/com/gen/colorslayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gen.colorslayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
